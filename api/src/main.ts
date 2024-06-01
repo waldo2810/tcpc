5d374c485f38cf12ddb254abe31f944d26550382
Waldo Sobrino <waldo2810@gmail.com>
2024-06-01 02:05:21 -0500

    fix: port

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3001);
+  await app
+    .listen(process.env.PORT || 3000, '0.0.0.0')
+    .then(() => console.log('Server is running on port', process.env.PORT));
 }
 bootstrap();
