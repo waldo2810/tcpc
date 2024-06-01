e2c69a277c8b26ed263bd858b6496b6173385a5f
Waldo Sobrino <waldo2810@gmail.com>
2024-05-31 23:01:17 -0500

    feat: add checkboxes

--- app/src/components/users-table/columns.tsx
+++ app/src/components/users-table/columns.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Checkbox } from "@radix-ui/react-checkbox";
 import { ColumnDef } from "@tanstack/react-table";
 import { Actions } from "./components/actions";
 import { UserProfile } from "./components/profile";
@@ -9,26 +8,33 @@ import { UserRating } from "./components/rating";
 import { UserRole } from "./components/role";
 import { SocialProfile } from "./components/social";
 import { UserStatus } from "./components/status";
+import { Checkbox } from "../ui/checkbox";
 
 export const columns: ColumnDef<User>[] = [
   {
     id: "select",
     header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
+      <div className="flex items-center justify-center">
+        <Checkbox
+          className="rounded-md border-gray-300 data-[state=checked]:bg-indigo-600 h-5 w-5"
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      </div>
     ),
     cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
+      <div className="flex items-center justify-center">
+        <Checkbox
+          className="rounded-md border-gray-300 data-[state=checked]:bg-indigo-600 h-5 w-5"
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      </div>
     ),
     enableSorting: false,
     enableHiding: false,
